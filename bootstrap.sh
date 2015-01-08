dd03a8bff6b094e94fb9facd794cc5debf8f74d1
bravefoot <b.v.campbell3@gmail.com>
2015-01-07 20:27:55 -0700

    fixed and finished basic bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
 sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 sed -i 's/us-east-1\.ec2\.//g' /etc/apt/sources.list
-apt-get update
-apt-get install apache2
+apt-get update -y
+apt-get install -y apache2
+apt-get install -y unzip
+wget https://github.com/bravefoot/cs462/archive/master.zip
+unzip master.zip
+rm /var/www/index.html
+cp cs462-master/name.txt /var/www/index.html

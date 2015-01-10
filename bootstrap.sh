b58aa8727dc617c84200ca781fd0b28200886f25
bravefoot <b.v.campbell3@gmail.com>
2015-01-10 16:47:40 -0700

    Updated bootstrap for Amazon linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
-sed -i 's/us-east-1\.ec2\.//g' /etc/apt/sources.list
-apt-get update -y
-apt-get install -y apache2
-apt-get install -y unzip
-wget https://github.com/bravefoot/cs462/archive/master.zip
-unzip master.zip
+#wget https://github.com/bravefoot/cs462/archive/master.zip
+#unzip master.zip
+#./cs462-master/bootstrap.sh
 rm /var/www/index.html
 cp cs462-master/name.txt /var/www/index.html

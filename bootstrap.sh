59b06ea9c00e4648f9e36afed569e1ab0efd2bd1
bravefoot <b.v.campbell3@gmail.com>
2015-01-10 17:25:00 -0700

    Fixed destination file in boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #wget https://github.com/bravefoot/cs462/archive/master.zip
 #unzip master.zip
-#./cs462-master/bootstrap.sh
+#sh ./cs462-master/bootstrap.sh
 rm /var/www/index.html
-cp cs462-master/name.txt /var/www/index.html
+cp cs462-master/name.txt /var/www/html/index.html
